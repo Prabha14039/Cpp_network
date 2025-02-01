3f508868d7098119ed890cc1a62fcf4425f29aaa
Prabha14039 <abhinavg760@gmail.com>
2025-02-01 06:30:47 +0530

    removed unecessary changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,8 @@
-#include"../include/graphic.h"
-#include"../include/server.h"
+#include "../include/glad.h"
+#include <GLFW/glfw3.h>
+
 int main()
 {
-    int size,width,height;
-    std::cout<<"Select window size\n 1. small \n 2.full";
-    std::cin>>size;
-    if(size == 1 )
-    {
-        height=800;
-        width=600;
-    }
 
-    Window win(height,width);
-    std::optional<int> window = win.createWindow();
-
-    if(window==-1)
-    {
-        return -1;
-    }
-    return 0;
 }
-/*   Server S(8080);
-     S.start();
-     */
+
